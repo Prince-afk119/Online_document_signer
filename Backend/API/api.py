b6812749afe6e3b49bdbcf899590288aea21e675
princedev <princedev@pycentric.co.za>
2026-07-30 14:31:03 +0200

    chore:lowercased backend folder name

--- Backend/API/api.py
+++ Backend/API/api.py
@@ -1,9 +1,9 @@
-from Backend.Database.signed_Document_DB import Store_Signed_Doc
+from backend.Database.signed_Document_DB import Store_Signed_Doc
 from pydantic import BaseModel
 import sys
 import os
 from pymongo import AsyncMongoClient
-from dotenv import load_dotenv,find_dotenv
+from dotenv import load_dotenv, find_dotenv
 from beanie import Document, init_beanie
 from datetime import datetime
 from fastapi import FastAPI, File, UploadFile, Form
@@ -19,26 +19,32 @@ user_name = os.environ.get("DB_USRNM")
 app = FastAPI(
     title="Document Signing API",
     version="1.0.0",
-    description="A simple API to upload and sign documents")
+    description="A simple API to upload and sign documents",
+)
+
 
 class Doc(BaseModel):
     email: str
     document_name: str
     content: bytes
 
+
 @app.post("/sign")
-async def sign_doc( email: str = Form(...),
+async def sign_doc(
+    email: str = Form(...),
     signature: str = Form(...),
     page: int = Form(...),
     x: float = Form(...),
-    y:float = Form(...),file: UploadFile = File(...)):
+    y: float = Form(...),
+    file: UploadFile = File(...),
+):
 
     try:
 
         bytePDF = await file.read()
-        document: fitz = fitz.open_stream(bytePDF,filetype="pdf")
+        document: fitz = fitz.open_stream(bytePDF, filetype="pdf")
 
-        #Signature placement
+        # Signature placement
 
         signed_doc = BytesIO()
         document.save(signed_doc)
@@ -46,13 +52,11 @@ async def sign_doc( email: str = Form(...),
 
         signed_content = signed_doc.getvalue()
 
-        await Store_Signed_Doc(
-            Dname = str(file.filename),
-            Dcontent = signed_content
-        )
+        await Store_Signed_Doc(Dname=str(file.filename), Dcontent=signed_content)
     except Exception as e:
         print(e)
         return {"error": str(e)}
+
 
 class Documents(Document):
     email: str
@@ -60,18 +64,16 @@ class Documents(Document):
     content: bytes
     uploaded_at: datetime = datetime.now()
 
-@app.post("/upload")
-async def Store_Doc(file: UploadFile = File(...),
-    email: str = Form(...)):
-     client :AsyncMongoClient = AsyncMongoClient(f"mongodb+srv://{user_name}:{password}@imscluster.mhsw47k.mongodb.net/")
 
-     await init_beanie(database=client["DocumentDB"], document_models=[Documents])
-     content = await file.read()
+@app.post("/upload")
+async def Store_Doc(file: UploadFile = File(...), email: str = Form(...)):
+    client: AsyncMongoClient = AsyncMongoClient(
+        f"mongodb+srv://{user_name}:{password}@imscluster.mhsw47k.mongodb.net/"
+    )
+
+    await init_beanie(database=client["DocumentDB"], document_models=[Documents])
+    content = await file.read()
 
     # Save to database
-     doc = Documents(
-        email=email,
-        document_name=file.filename,
-        content=content
-    )
-     await doc.insert()
+    doc = Documents(email=email, document_name=file.filename, content=content)
+    await doc.insert()
--- Backend/Database/Signed_Document_DB.py
+++ Backend/Database/Signed_Document_DB.py
@@ -7,19 +7,22 @@ load_dotenv(find_dotenv())
 password = os.environ.get("DB_PSWRD")
 user_name = os.environ.get("DB_USRNM")
 
+
 class Document(Doc):
     document_name: str
     content: bytes
 
 
-async def Store_Signed_Doc(Dname: str,Dcontent:bytes):
-     client :AsyncMongoClient = AsyncMongoClient(f"mongodb+srv://{user_name}:{password}@imscluster.mhsw47k.mongodb.net/")
+async def Store_Signed_Doc(Dname: str, Dcontent: bytes):
+    client: AsyncMongoClient = AsyncMongoClient(
+        f"mongodb+srv://{user_name}:{password}@imscluster.mhsw47k.mongodb.net/"
+    )
 
-     await init_beanie(database=client["DocumentDB"], document_models=[Document])
+    await init_beanie(database=client["DocumentDB"], document_models=[Document])
 
-     updates: dict = {}
-     updates["content"] = Dcontent
-     document = await Document.find_one({"document_name":Dname})
-     if document:
-         await document.update({"$set": updates})
-         print("Document updated:", document.document_name)
+    updates: dict = {}
+    updates["content"] = Dcontent
+    document = await Document.find_one({"document_name": Dname})
+    if document:
+        await document.update({"$set": updates})
+        print("Document updated:", document.document_name)
